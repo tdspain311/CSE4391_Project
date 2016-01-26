9a3c0f2e6a4d1dded462fa25904c063b0285ae8a
Tyler D'Spain <vandyke666@gmail.com>
2016-01-25 22:58:55 -0600

    added full point cloud test site- located temp/potree/examples

---
+++ Web/temp/test_chair.ply_converted/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 40128,
+    "boundingBox": {
+        "lx": 0.0,
+        "ly": 0.0,
+        "lz": 0.0,
+        "ux": 0.0,
+        "uy": 0.0,
+        "uz": 0.0
+    },
+    "tightBoundingBox": {
+        "lx": 0.0,
+        "ly": 0.0,
+        "lz": 0.0,
+        "ux": 0.0,
+        "uy": 0.0,
+        "uz": 0.0
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.0,
+    "scale": 0.0001,
+    "hierarchyStepSize": 5
+}
