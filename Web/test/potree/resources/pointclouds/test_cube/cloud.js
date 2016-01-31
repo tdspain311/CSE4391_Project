f5f32fabe66c7e68ad2e80a9dfb78fab500dd8f8
Tyler D'Spain <vandyke666@gmail.com>
2016-01-31 14:30:26 -0600

    Seperated out testing with webGL and potree sandboxes

---
+++ Web/test/potree/resources/pointclouds/test_cube/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 8,
+    "boundingBox": {
+        "lx": -1.6315038204193116,
+        "ly": -1.6315853595733643,
+        "lz": 0.0,
+        "ux": 1.631666898727417,
+        "uy": 1.6315853595733643,
+        "uz": 3.2631707191467287
+    },
+    "tightBoundingBox": {
+        "lx": -1.6315038204193116,
+        "ly": -1.6315853595733643,
+        "lz": 0.0,
+        "ux": 1.6315038204193116,
+        "uy": 1.6315853595733643,
+        "uz": 3.263007640838623
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.028259888291358949,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
